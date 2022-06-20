e9e27424713333f355e6102c8c0adf179c6c0cb4
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-06-20 12:07:09 +0530

    nav final

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,37 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+
 import "../style/Nav.css";
 
 const Nav = () => {
   return (
     <div className="Nav">
-      <h1>nav</h1>
+      <ul>
+        <li className="da">
+          <NavLink to="/dashboard">
+            <div className="img"></div>
+            <span>Dashboard</span>
+          </NavLink>
+        </li>
+        <li className="do">
+          <NavLink to="/doctor">
+            <div className="img"></div>
+            <span>Doctor</span>
+          </NavLink>
+        </li>
+        <li className="co">
+          <NavLink to="/contact">
+            <div className="img"></div>
+            <span>Contact</span>
+          </NavLink>
+        </li>
+        <li className="me">
+          <NavLink to="/medicine">
+            <div className="img"></div>
+            <span>Medicine</span>
+          </NavLink>
+        </li>
+      </ul>
     </div>
   );
 };
